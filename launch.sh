1deddb89c622bfd0015590932e82d896ba2b0c79
tkgsy <meirvg@gmail.com>
2019-05-29 11:28:18 +0900

    Remove stop

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-i386 -S -gdb tcp::30012 -m 2 -localtime -vga std -hda "$1" -monitor stdio
+qemu-system-i386 -gdb tcp::30012 -m 2 -localtime -vga std -hda "$@" -monitor stdio
